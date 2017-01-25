cbd00e73f6e4af60283783a437421914b4e3fc68
lverno321 <lverno321@gmail.com>
2017-01-25 18:40:53 -0500

    Added the BallThrower Class.
    The BallThrower class is just a simple class that shoots the ball whenn
    you press x.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallThrower.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by lvern on 1/25/2017.
+ */
+
+@TeleOp(name="BallThrower")
+public class BallThrower extends RobotHardware {
+
+    public void waitFor(int time) {
+        try{
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override public void loop() {
+
+        if(gamepad1.x) {
+
+            shooterMotor.setPower(1);
+            waitFor(500);
+            shooterMotor.setPower(0);
+        }
+
+    }
+
+}
