9e6bee312576617746055b26285c9e18c3b28314
lverno321 <lverno321@gmail.com>
2017-09-17 15:12:38 -0400

    Created class "Gyro"
    Read the title.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by lvern on 9/17/2017.
+ */
+
+
+import com.qualcomm.robotcore.hardware.GyroSensor;
+
+public class Gyro extends RobotHardware {
+
+    @Override public void init() {
+        try {
+            gyroSensor = hardwareMap.gyroSensor.get("gyroSensor");
+            gyroSensor.calibrate();
+        } catch (Exception e) {
+            telemetry.addData("error", "gyro sensor");
+            gyroSensor = null;
+        }
+    }
+
+}
