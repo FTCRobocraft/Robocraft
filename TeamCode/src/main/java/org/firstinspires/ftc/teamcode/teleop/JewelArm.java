ea84782582e5d7fb065faee5eebd5272fbb50989
lverno321 <lverno321@gmail.com>
2017-10-15 16:14:06 -0400

    Finished the JewelArm class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/JewelArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/JewelArm.java
@@ -10,9 +10,26 @@ import org.firstinspires.ftc.teamcode.util.RobotHardware;
 @TeleOp(name = "JewelArm")
 public class JewelArm extends RobotHardware {
 
+
+    boolean manual = true;
+    JewelAutoStages currentStage = JewelAutoStages.Down;
+    double endTime;
+    boolean init = true;
+    boolean forward;
+
     boolean isRed;
     double downPosition = 0.0;
     double upPosition = 90.0;
+    final int sensitivity = 100;
+    double servoWaitTime = 1000;
+    double moveWaitTime = 2000;
+
+    enum JewelAutoStages {
+        Down,
+        Move,
+        Move2,
+        Up
+    }
 
     @Override public void init() {
         super.init();
@@ -20,12 +37,83 @@ public class JewelArm extends RobotHardware {
 
     @Override public void loop() {
 
-        if (gamepad2.x) {
-            armServo.setPosition(downPosition);
-            if (colorSensor.red() > 10) {moveForward(0.3f);} else {moveBackward(0.3f);}
-            armServo.setPosition(upPosition);
+        if (manual) {
+            if (gamepad2.x) {
+                manual = false;
+                armServo.setPosition(downPosition);
+                if (colorSensor.red() > sensitivity) {
+                    moveForward(0.3f);
+                } else {
+                    moveBackward(0.3f);
+                }
+                armServo.setPosition(upPosition);
+            }
+        } else {
+            switch (currentStage) {
+                case Down:
+                    if (init) {
+                        armServo.setPosition(downPosition);
+                        endTime = System.currentTimeMillis() + servoWaitTime;
+                        init = false;
+                    }
+                    if (System.currentTimeMillis() >= endTime) {
+                        init = true;
+                        currentStage = JewelAutoStages.Move;
+
+                    }
+                    break;
+                case Move:
+                    if (init) {
+                        init = false;
+                        if (colorSensor.red() > sensitivity) {
+                            forward = true;
+                            moveForward(0.3f);
+                            endTime = System.currentTimeMillis() + moveWaitTime;
+                        } else {
+                            forward = false;
+                            moveBackward(0.3f);
+                            endTime = System.currentTimeMillis() + moveWaitTime;
+                        }
+
+                    }
+                    if (System.currentTimeMillis() >= endTime) {
+                        init = true;
+                        currentStage = JewelAutoStages.Move2;
+
+                    }
+                    break;
+                case Move2:
+                    if (init) {
+                        init = false;
+                        if (forward) {
+                            moveBackward(0.3f);
+                            endTime = System.currentTimeMillis() + moveWaitTime;
+                        } else {
+                            moveForward(0.3f);
+                            endTime = System.currentTimeMillis() + moveWaitTime;
+                        }
+                    }
+                    if (System.currentTimeMillis() >= endTime) {
+                        init = true;
+                        currentStage = JewelAutoStages.Up;
+
+                    }
+                    break;
+                case Up:
+                    if (init) {
+                        armServo.setPosition(upPosition);
+                        endTime = System.currentTimeMillis() + servoWaitTime;
+                        init = false;
+                    }
+                    if (System.currentTimeMillis() >= endTime) {
+                        init = true;
+                        currentStage = JewelAutoStages.Down;
+                        manual = true;
+                    }
+                    break;
+
+            }
         }
-
-
     }
+
 }
