3876b5432956b91ef535ae49ed7d3758505e55f5
lverno321 <lverno321@gmail.com>
2017-11-11 18:53:50 -0500

    Created new actions and added them to the sequence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/sequences/JewelActionSequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/sequences/JewelActionSequence.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.autonomous.sequences;
 
 import com.qualcomm.robotcore.robot.Robot;
 
+import org.firstinspires.ftc.teamcode.action.MecanumMoveAction;
 import org.firstinspires.ftc.teamcode.action.ServoAction;
 import org.firstinspires.ftc.teamcode.action.WaitAction;
 import org.firstinspires.ftc.teamcode.util.ActionSequence;
@@ -15,6 +16,7 @@ public class JewelActionSequence extends ActionSequence {
 
     double lowPos = 0;
     double hiPos = 0.7; //0.??
+    int colorSensitivity = 3;
 
     public JewelActionSequence(RobotHardware.Team team) {
 
@@ -22,6 +24,7 @@ public class JewelActionSequence extends ActionSequence {
             case Red:
                 addAction(new WaitAction(1));
                 addAction(new ServoAction(0));
+                addAction(new MecanumMoveAction(RobotMoveDirection.FORWARD, 5, 0.5f, 2));
                 break;
 
             case Blue:
