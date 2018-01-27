52f246f436069b61646427c789f16f1aa109b2ac
lverno321 <lverno321@gmail.com>
2018-01-26 19:31:44 -0500

    very minor and unimportant things for my benefit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Manual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Manual.java
@@ -1,12 +1,12 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.firstinspires.ftc.teamcode.util.RobotHardware;
 
 /**
  * Created by lvern on 1/6/2018.
  */
+
 @TeleOp(name="Manual")
 public class Manual extends RobotHardware {
 
