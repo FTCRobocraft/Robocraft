be616994cdf4fd7d09b37731faa2ad5bd3d767b2
lverno321 <lverno321@gmail.com>
2018-01-26 20:02:05 -0500

    manual

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Manual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Manual.java
@@ -14,8 +14,7 @@ public class Manual extends RobotHardware {
     float bumperPower = 1f;
     float dpadGripperPower = 1f;
 
-    float slowModeMove = 2f;
-    float slowModeRotate = 1f;
+    float slowModeSpeed = 1f;
 
     double clawElbowUp = 0;
     double clawElbowDown = 1;
@@ -36,12 +35,10 @@ public class Manual extends RobotHardware {
             if (!slowToggle) {
                 if (slowToggleState) {
                     slowToggleState = false;
-                    slowModeMove = 4f;
-                    slowModeRotate = 2f;
+                    slowModeSpeed = 2f;
                 } else {
                     slowToggleState = true;
-                    slowModeMove = 2f;
-                    slowModeRotate = 1f;
+                    slowModeSpeed = 1f;
                 }
                 slowToggle = true;
             }
@@ -50,41 +47,41 @@ public class Manual extends RobotHardware {
         }
 
         if (gamepad1.dpad_up) {
-            moveForward(dpadPower);
+            moveForward(dpadPower / slowModeSpeed);
         }
         else if (gamepad1.dpad_down) {
-            moveBackward(dpadPower);
+            moveBackward(dpadPower / slowModeSpeed);
         }
         else if (gamepad1.dpad_left) {
-            moveLeft(dpadPower);
+            moveLeft(dpadPower / slowModeSpeed);
         }
         else if (gamepad1.dpad_right) {
-            moveRight(dpadPower);
+            moveRight(dpadPower / slowModeSpeed);
         }
 
         if (gamepad1.right_stick_x > 0 && gamepad1.right_stick_y > 0) { // Forward Right
-            moveForwardRight((float) ((gamepad1.right_stick_x + gamepad1.right_stick_y) / slowModeMove));
+            moveForwardRight((float) ((gamepad1.right_stick_x + gamepad1.right_stick_y) / slowModeSpeed));
         } else if (gamepad1.right_stick_x > 0 && gamepad1.right_stick_y < 0) { // Backward Right
-            moveBackwardRight((float) ((gamepad1.right_stick_x + gamepad1.right_stick_y) / slowModeMove));
+            moveBackwardRight((float) ((gamepad1.right_stick_x + gamepad1.right_stick_y) / slowModeSpeed));
         } else if (gamepad1.right_stick_x < 0 && gamepad1.right_stick_y > 0) { // Forward Left
-            moveForwardLeft((float) ((gamepad1.right_stick_x + gamepad1.right_stick_y) / slowModeMove));
+            moveForwardLeft((float) ((gamepad1.right_stick_x + gamepad1.right_stick_y) / slowModeSpeed));
         } else if (gamepad1.right_stick_x < 0 && gamepad1.right_stick_y < 0) { // Backward Left
-            moveBackwardLeft((float) ((gamepad1.right_stick_x + gamepad1.right_stick_y) / slowModeMove));
+            moveBackwardLeft((float) ((gamepad1.right_stick_x + gamepad1.right_stick_y) / slowModeSpeed));
         }
 
 
         if (gamepad1.right_trigger > 0) {
-            rotateRight(gamepad1.right_trigger / slowModeRotate);
+            rotateRight(gamepad1.right_trigger / slowModeSpeed);
         }
         else if (gamepad1.left_trigger > 0) {
-            rotateLeft(gamepad1.left_trigger / slowModeRotate);
+            rotateLeft(gamepad1.left_trigger / slowModeSpeed);
         }
 
         if (gamepad1.right_bumper) {
-            rotateRight(bumperPower / slowModeRotate);
+            rotateRight(bumperPower / slowModeSpeed);
         }
         else if (gamepad1.left_bumper) {
-            rotateLeft(bumperPower / slowModeRotate);
+            rotateLeft(bumperPower / slowModeSpeed);
         }
         //endregion
 
@@ -119,7 +116,7 @@ public class Manual extends RobotHardware {
                     lift_gripServo.setPosition(m_liftGripClosed);
                 } else {
                     liftClawState = true;
-                    clawElbowServo.setPosition(m_liftGripOpen);
+                    lift_gripServo.setPosition(m_liftGripOpen);
                 }
                 leftTriggerPressed = true;
             }
