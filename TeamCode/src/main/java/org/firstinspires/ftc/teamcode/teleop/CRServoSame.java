8bf8d0be908ab853655c075024204335ef1283b9
lverno321 <lverno321@gmail.com>
2019-01-01 09:54:01 -0500

    Added more CR Servo test programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/CRServoSame.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/CRServoSame.java
@@ -3,16 +3,16 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.hardware.TestHardware;
 
-@TeleOp(name="CRServoTest")
-public class CRServoTest extends TestHardware {
 
-    @Override
-    public void loop() {
+@TeleOp(name="CRServoSame")
+public class CRServoSame extends TestHardware {
+
+    @Override public void loop() {
         if (gamepad1.a) {
-            CRServoTest1.setPower(-1);
+            CRServoTest1.setPower(1);
             CRServoTest2.setPower(1);
         } else if (gamepad1.y) {
-            CRServoTest1.setPower(1);
+            CRServoTest1.setPower(-1);
             CRServoTest2.setPower(-1);
         } else {
             CRServoTest1.setPower(0);
