6e88b69f9a6375e5f5fb589f417ca5cbcd9a860e
lverno321 <lverno321@gmail.com>
2019-09-20 17:35:01 -0400

    Update Skystone stuff
    Added motors and omniDrive to SkyStoneHardware and added circleMove functionality to SkyStoneTeleOp.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/SkyStoneHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/SkyStoneHardware.java
@@ -1,9 +1,36 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.util.OmniDrive;
 
 public class SkyStoneHardware extends BaseHardware {
 
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;
+    public DcMotor backRight;
+    public OmniDrive omniDrive;
 
+    @Override
+    public void init() {
+        super.init();
 
+        try {
+            frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+            frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+
+            backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+            backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+            omniDrive = new OmniDrive(frontLeft, frontRight, backLeft, backRight);
+        } catch (Exception e) {
+            telemetry.addData("Not Found:", e.getMessage());
+        }
+    }
 }
